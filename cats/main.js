0b7b45d9e7e9aa7828e792aa6d1d6fccc868dbf7
Giuditta <giudittamarino89@gmail.com>
2021-03-22 14:26:14 +0100

    milestone2

--- cats/main.js
+++ cats/main.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 
   // Tramite la funzione .forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
   cats.forEach((element) => {
-    console.log(`Il gatto dal nome ${element.nome} è di colore ${element.colore}`);
+    $('#listaGatti').append(`<li> Il gatto ${element.nome} è di colore ${element.colore}`);
   });
 
   // Milestone 2
@@ -52,114 +52,32 @@ $(document).ready(function(){
   console.log(mCats);
   console.log(fCats);
   console.log($('#listaGatti'));
-  // e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio
-  // se il gatto ha tre o meno anni, il fiocco avrà opacità più tenue
-  cats.forEach((element) => {
-
-    if (element.sesso == 'maschio') {
-      if (element.eta <= 3) {
-          $('#listaGatti').append(`<li> ${element.nome} <i class="fas fa-ribbon blu light"></i> </li>`);
-      } else {
-          $('#listaGatti').append(`<li> ${element.nome} <i class="fas fa-ribbon blu"></i> </li>`);
-      }
-    } else {
-      if (element.eta <= 3) {
-          $('#listaGatti').append(`<li> ${element.nome} <i class="fas fa-ribbon rosa light"></i> </li>`);
-      } else {
-          $('#listaGatti').append(`<li> ${element.nome} <i class="fas fa-ribbon rosa"></i> </li>`);
-        }
-      }
-    });
-
-  // Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio,
-  // inserendo solamente nome e colore e colore e opacità del fiocco per ogni gatto.
-  const newCats = [
-  {
-    nome: 'micio',
-    eta: 5,
-    colore: 'maculato',
-    sesso: 'maschio'
-  },
-  {
-    nome: 'teo',
-    eta: 3,
-    colore: 'bianco',
-    sesso: 'maschio'
-  },
-  {
-    nome: 'luna',
-    eta: 1,
-    colore: 'nero',
-    sesso: 'femmina'
-  },
-  {
-    nome: 'giada',
-    eta: 10,
-    colore: 'rosso',
-    sesso: 'femmina'
-  },
-  {
-    nome: 'tigre',
-    eta: 7,
-    colore: 'tigrato',
-    sesso: 'femmina'
-  }
-  ];
-
-
-  // prendere l'array di oggetti e creare un array con solo il nome dei gatti
-
-  let newMCats = newCats.filter((element) => element.sesso == 'maschio');
-  let newFCats = newCats.filter((element) => element.sesso == 'femmina');
-  console.log(newMCats);
-  console.log(newFCats);
-
-
-  var newMCatsNome = [];
-
-  for (var i = 0; i < newMCats.length; i++) {
-    newMCatsNome.push((newMCats[i]['nome']));
-  }
-
-  console.log(newMCatsNome);
-
-  var newFCatsNome = [];
-
-  for (var i = 0; i < newFCats.length; i++) {
-    newFCatsNome.push((newFCats[i]['nome']));
-  }
-  console.log(newFCatsNome);
-  let color;
+  // e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio  
+    // se il gatto ha tre o meno anni, il fiocco avrà opacità più tenue
+  let coloreFiocco;
   let opacity;
-  newCats.forEach((element, i , array) => {
-    console.log(array);
 
+  cats.forEach((element) => {
     if (element.sesso == 'maschio') {
-      color = 'blu';
-      if (element.eta <= 3) {
-        opacity = 'light';
-      } else {
-        opacity = 'dark';
-      }
+      coloreFiocco = 'blu'
+        if (element.eta <= 3) {
+          opacity = 'light'
+        } else {
+          opacity = 'dark'
+        }
     } else {
-      color = 'rosa';
-      if (element.eta <= 3) {
-        opacity = 'light';
-      } else {
-        opacity = 'dark';
-      }
-    }
+      coloreFiocco = 'rosa'
+        if (element.eta <= 3) {
+          opacity = 'light'
+        } else {
+          opacity = 'dark'
+        }
+    };
+
+    $('#listaGattiDue').append(`<li> Gatto ${element.nome} <i class="fas fa-ribbon ${coloreFiocco} ${opacity}"></i></li>`)
   });
 
-  const mergedCats = newCats.map((element, i, array) =>{
-    let obj = {
-      ... element,
-      color,
-      opacity
-    }
-    return obj
-  })
-  console.log(mergedCats);
+
 
 
 
